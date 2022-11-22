db2846794d373ffa2637fc07043289c844dcee9c
JoonasPitk <46161@edu.raseko.fi>
2022-11-22 14:28:28 +0200

    Start INSERT, plus formatting.

--- pgModule.py
+++ pgModule.py
@@ -17,15 +17,15 @@ class DatabaseOperation():
     # Constructor method: create a new object and set initial properties
     def __init__(self):
         self.errorCode = 0
-        self.errorMessage = 'OK'
-        self.detailedMessage = 'No errors'
+        self.errorMessage = "OK"
+        self.detailedMessage = "No errors"
         self.resultSet = []
         self.columnHeaders = []
         self.rows = 0
         self.columns = 0
 
     # Method for creating connection arguments
-    def createConnectionArgumentDict(self, database, role, pwd, host='localhost', port='5432',):
+    def createConnectionArgumentDict(self, database, role, pwd, host="localhost", port="5432"):
         """Creates a dictionary from connection arguments
 
         Args:
@@ -39,28 +39,28 @@ class DatabaseOperation():
             dict: Connection arguments as key-value pairs
         """
         connectionArgumentDict = {}
-        connectionArgumentDict['server'] = host
-        connectionArgumentDict['port'] = port
-        connectionArgumentDict['database'] = database
-        connectionArgumentDict['user'] = role
-        connectionArgumentDict['password'] = pwd
+        connectionArgumentDict["server"] = host
+        connectionArgumentDict["port"] = port
+        connectionArgumentDict["database"] = database
+        connectionArgumentDict["user"] = role
+        connectionArgumentDict["password"] = pwd
 
         return connectionArgumentDict
 
     # Method for saving connection arguments to a settings file
-    def saveDatabaseSettingsToFile(self, file, connectionArgs,):
+    def saveDatabaseSettingsToFile(self, file, connectionArgs):
         """Saves connection arguments to JSON based settings file
 
         Args:
             file (str): Name of the JSON settings file
             connectionArgs (dict): Connection arguments in key-value pairs
         """
-        settingsFile = open(file, 'w',)
+        settingsFile = open(file, "w",)
         json.dump(connectionArgs, settingsFile,)
         settingsFile.close()
 
     # Method for reading connection arguments from the settings file
-    def readDatabaseSettingsFromFile(self, file,):
+    def readDatabaseSettingsFromFile(self, file):
         """Reads connection arguments from JSON based settings file
 
         Args:
@@ -75,30 +75,30 @@ class DatabaseOperation():
         return connectionArgumentDict
 
     # Method to get all rows from a given table
-    def getAllRowsFromTable(self, connectionArgs, table,):
+    def getAllRowsFromTable(self, connectionArgs, table):
         """Selects all rows from the table
 
         Args:
             connectionArgs (dict): Connection arguments in key-value pairs
             table (str): Name of the table to read from
         """
-        server = connectionArgs['server']
-        port = connectionArgs['port']
-        database = connectionArgs['database']
-        user = connectionArgs['user']
-        password = connectionArgs['password']
+        server = connectionArgs["server"]
+        port = connectionArgs["port"]
+        database = connectionArgs["database"]
+        user = connectionArgs["user"]
+        password = connectionArgs["password"]
 
         try:
             # Connect to the database and set error parameters
             dbconnection = psycopg2.connect(
-                database=database, user=user, password=password, host=server, port=port,)
+                database=database, user=user, password=password, host=server, port=port)
             self.errorCode = 0
-            self.errorMessage = 'Yhdistettiin tietokantaan'
-            self.detailedMessage = 'Connected to database successfully'
+            self.errorMessage = "Yhdistettiin tietokantaan"
+            self.detailedMessage = "Connected to database successfully"
 
             # Create a cursor to retrieve data from the table
             with dbconnection.cursor() as cursor:
-                sqlClause = 'SELECT * FROM ' + table + ';'
+                sqlClause = "SELECT * FROM " + table + ";"
                 cursor.execute(sqlClause)
 
                 # Set object properties
@@ -109,33 +109,66 @@ class DatabaseOperation():
 
                 # Set error values
                 self.errorCode = 0
-                self.errorMessage = 'Luettiin taulu onnistuneesti'
-                self.detailedMessage = 'Read all data from the table'
+                self.errorMessage = "Luettiin taulu onnistuneesti"
+                self.detailedMessage = "Read all data from the table"
 
         except (Exception, psycopg2.Error )as error:
 
             # Set error values
             self.errorCode = 1
-            self.errorMessage = 'Tietokannan käsittely ei onnistunut'
+            self.errorMessage = "Tietokannan käsittely ei onnistunut"
             self.detailedMessage = str(error)
 
         finally:
             if self.errorCode == 0:
                 dbconnection.close()
     
-    # TODO: Finish writing methods for insert, update and delete.
+    # TODO: Finish writing methods for INSERT, UPDATE and DELETE.
     # Method to insert a row to a given table
-    def insertRowToTable(self, connectionArgs, sqlClause,):
+    def insertRowToTable(self, connectionArgs, sqlClause):
         """Inserts a row to table according to a SQL clause
 
         Args:
             connectionArgs (dict): Connection arguments in key-value pairs
             sqlClause (str): Insert clause
         """
-        pass
+        server = connectionArgs["server"]
+        port = connectionArgs["port"]
+        database = connectionArgs["database"]
+        user = connectionArgs["user"]
+        password = connectionArgs["password"]
+
+        try:
+            # Connect to the database and set error parameters
+            dbconnection = psycopg2.connect(
+                database=database, user=user, password=password, host=server, port=port)
+            self.errorCode = 0
+            self.errorMessage = "Yhdistettiin tietokantaan"
+            self.detailedMessage = "Connected to database successfully"
+
+            # Create a cursor to retrieve data from the table
+            with dbconnection.cursor() as cursor:
+                cursor.execute(sqlClause)
+
+                # Set error values
+                self.errorCode = 0
+                self.errorMessage = "Lisättiin tietue onnistuneesti"
+                self.detailedMessage = "Inserting into table was successful"
+                dbconnection.commit()
+
+        except (Exception, psycopg2.Error)as error:
+
+            # Set error values
+            self.errorCode = 1
+            self.errorMessage = "Tietokannan käsittely ei onnistunut"
+            self.detailedMessage = str(error)
+
+        finally:
+            if self.errorCode == 0:
+                dbconnection.close()
 
     # Method to update a table
-    def updateTable(self, connectionArgs, table, column, limit,):
+    def updateTable(self, connectionArgs, table, column, limit):
         """Updates a table
 
         Args:
@@ -147,7 +180,7 @@ class DatabaseOperation():
         pass
 
     # Method to delete a row from table
-    def deleteFromTable(self, connectionArgs, table, limit,):
+    def deleteFromTable(self, connectionArgs, table, limit):
         """Delete rows from a table using limiting SQL statement
 
         Args:
@@ -166,19 +199,19 @@ if __name__ == "__main__":
 
     # Create a dictionary for connection settings using defaults
     dictionary = testOperation.createConnectionArgumentDict(
-        'metsastys', 'sovellus', 'Q2werty')
+        "metsastys", "sovellus", "Q2werty")
     # FIXME: Correct this line.
-    '''
+    """
     print(dictionary)
-    '''
+    """
     
     # Save those settings to a file
-    testOperation.saveDatabaseSettingsToFile('settings.dat', dictionary,)
+    testOperation.saveDatabaseSettingsToFile("settings.dat", dictionary)
 
     # Read settings back from the file
-    readedSettings = testOperation.readDatabaseSettingsFromFile('settings.dat',)
+    readedSettings = testOperation.readDatabaseSettingsFromFile("settings.dat")
 
     # print(readedSettings)
-    testOperation.getAllRowsFromTable(readedSettings, 'public.jasen',)
+    testOperation.getAllRowsFromTable(readedSettings, "public.jasen")
 
     print(testOperation.resultSet)
